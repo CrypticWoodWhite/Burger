c19ffe6eb5c7314879af4022db584dd1814cb233
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-03-24 18:15:49 -0700

    controllers done

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var burger = require("../models/burger");
+
+var router = express.Router();
+
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+  
+router.post("/api/burgers", function(req, res) {
+    burger.create(["name", "devoured"], [req.body.buger_name, req.body.devoured], function(res) {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+  
+router.put("/api/burgers/:id", function(req, res) {
+    var cond = "id = " + req.params.id;
+  
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        cond,
+        function(res) {
+            if (res.changedRows === 0) {
+            return res.status(404).end();
+            }
+        res.status(200).end();
+        }
+    );
+  });
+  
+  module.exports = router;
